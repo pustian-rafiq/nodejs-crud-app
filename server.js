a2359585f3d50f9f2b9353587021b0ab734de65e
pustian-rafiq <rafiqul.pust.cse@gmail.com>
2022-11-19 19:35:23 +0600

    setup required modules

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+//Configure dotenv
+dotenv.config({path:'config.env'});
+const PORT = process.env.PORT || 8000;
+
+//Log request
+app.use(morgan('tiny'));
+
+//parse request to body-parser
+app.use(bodyParser.urlencoded({extended: true}));
+
+//set view engine
+app.set('view engine','ejs')
+// app.set("views", path.resolve(__dirname."views/ejs"))
+
+//load assets
+app.use('/css',express.static(path.resolve(__dirname,"assets/css")));
+app.use('/image',express.static(path.resolve(__dirname,"assets/image")));
+app.use('/js',express.static(path.resolve(__dirname,"assets/js")));
+
+app.get('/', (req,res) => {
+    res.send("Nodejs CRUD Application");
+});
+
+
+//Listen the server
+app.listen(3000, ()=>{
+    console.log(`Project is running on port ${PORT}`);
+})
